a17e7fa87928a66f488261f01d7310965ed23151
Tyler Palma <tyler@westwardstudios.com>
2022-05-20 14:27:46 -0400

    rename userscript

--- pos.js
+++ pos.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Ra-Mtek-POS
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
