a057a4ac03ef033e2d9a68203bfc5b1f7467a2a7
Tyler Palma <tyler@westwardstudios.com>
2022-06-09 09:31:24 -0400

    run on admin load in

--- pos.js
+++ pos.js
@@ -4,7 +4,8 @@
 // @version      0.3.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://rayoga.marianatek.com/admin/cart/*
+// @match        https://rayoga.marianatek.com/*
+// @exclude      https://rayoga.marianatek.com/products/*
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/tylerpalma/ra-mtek-tamper/main/pos.css
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        GM_getResourceText
