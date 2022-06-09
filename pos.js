3c13b83fe5b3b6d10e3680d17661dba9941dd018
Tyler Palma <tyler@westwardstudios.com>
2022-06-09 09:34:24 -0400

    version bump

--- pos.js
+++ pos.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ra-Mtek-POS
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://rayoga.marianatek.com/*
